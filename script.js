11cdc95a0560963b52cddb99a93029bec356ea74
Henna <henna.tuominen@tuni.fi>
2022-01-02 13:48:44 +0200

    Fixed bug in header.toggle

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+
 $(document).ready(function(){
   $('#menu').click(function(){
     $(this).toggleClass('fa-times');
     $('header').toggleClass('toggle');
   })
+  $('.navbar-link').click(function(){
+    $('header').toggleClass('toggle');
+    $('#menu').toggleClass('fa-times');
+  });
 })
 
-$(document).ready(function(){
-  $('#navbar-link').click(function(){
-    $('header').toggleClass('toggle');
-  })
-})
