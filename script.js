a601abee8c7d9cf2be0b686b8b82c0199abd45fb
Henna <henna.tuominen@tuni.fi>
2022-01-11 14:17:31 +0200

    Add popup for Satoja project

--- script.js
+++ script.js
@@ -8,5 +8,15 @@ $(document).ready(function(){
     $('header').toggleClass('toggle');
     $('#menu').toggleClass('fa-times');
   });
-})
+  $('#more-btn').click(function(){
+    $('#project-popup').toggleClass('project-popup');
+    $('#close-popup').toggleClass('toggle');
+    $('#popup-text').toggleClass('toggle');
 
+  })
+  $('#close-popup').click(function(){
+    $('#project-popup').toggleClass('project-popup');
+    $('#close-popup').toggleClass('toggle');
+    $('#popup-text').toggleClass('toggle');
+  })
+})
